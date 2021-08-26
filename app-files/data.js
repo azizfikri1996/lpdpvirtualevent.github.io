7ff1ce29bd0f7c93904bc17f2622a8fff89b3cd1
azizfikri1996 <39547510+azizfikri1996@users.noreply.github.com>
2021-08-26 10:58:48 +0700

    initial commit

---
+++ app-files/data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lobby-area-view-1",
+      "name": "Lobby Area View 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.314409940839699,
+        "pitch": 0,
+        "fov": 1.3460094916088818
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1822143313176596,
+          "pitch": 0.04455948942844934,
+          "rotation": 0,
+          "target": "0-lobby-area-view-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Lobby Area 1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
